49104eddd1403b7aaf9aa91ca471fedcf19583ec
Maxim Yefremov <maxYefr@gmail.com>
2018-07-27 09:19:57 +0300

    create gcloud cert

--- ssl.sh
+++ ssl.sh
@@ -4,4 +4,19 @@ set -ex
 create(){
 	openssl req -config ./openssl.conf -x509 -nodes -days 365 -newkey rsa:2048 -keyout selfsigned.key -out selfsigned.crt
 }
+
+create_gcloud(){
+	gcloud compute ssl-certificates create my-test-ssl-cert \
+	    --certificate selfsigned.crt \
+	    --private-key selfsigned.key
+}
+
+describe(){
+	gcloud compute ssl-certificates describe my-test-ssl-cert
+}
+
+list(){
+	gcloud compute ssl-certificates list
+}
+
 $@
